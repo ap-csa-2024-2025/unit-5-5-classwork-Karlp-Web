b01d9bce1fd936874bf7ac65a3a6086ad249009c
Karlp-Web <karlenportillo1@gmail.com>
2024-12-09 19:13:00 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -4,18 +4,26 @@ public class Main
 {
   public static void main(String[] args)
   {
+//Point2D example
     Point2D p1 = new Point2D();
     Point2D p2 = new Point2D();
 
+  //No longer allowed when in private
     p1.x = 0;
     p1.y = 1;
 
     p2.x = 1;
     p2.y = 0;
+  //is able to use the function
+    p1.setX(5);
+    p2.setX(10);
 
+//Dog example
     Dog d = new Dog();
     d.breed = "Corgi";
     d.size = 5.0;
     d.colorOfFur = "Blue";
+
+//  Example
   }
 }
--- Point2D.java
+++ Point2D.java
@@ -2,4 +2,14 @@ public class Point2D
 {
     private int x;
     private int y;
+
+    public void setX(int inputX)
+    {
+        x = inputX;
+    }
+
+    Public void setY(int inputY)
+    {
+        y = inputY;
+    }
 }
